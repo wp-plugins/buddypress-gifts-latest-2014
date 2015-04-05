80aa5e31e576943c1b3b11021af7a4ecf408b626
amitray <amitray@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-05 18:11:17 +0000

    Changed points to accept decimals
    git-svn-id: https://plugins.svn.wordpress.org/buddypress-gifts-latest-2014/tags/1.5@1128288 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/templates/gifts/screen.php
@@ -0,0 +1,473 @@
+<?php get_header('buddypress') ?>
+	<div id="main-content" class="main-content">
+    <div id="primary" class="content-area">
+	<div id="content" class="site-content" role="main">
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<div class="entry-content">
+			<div id="buddypress">
+
+            <h1 class="entry-title">
+			<a href="<?php bp_displayed_user_link(); ?>" rel="bookmark"><?php bp_displayed_user_mentionname(); ?></a>
+			</h1>
+                
+            	<?php do_action( 'bp_before_member_header' ); ?>
+
+                <div id="item-header-avatar">
+                    <a href="<?php bp_displayed_user_link(); ?>">
+                
+                        <?php bp_displayed_user_avatar( 'type=full' ); ?>
+                
+                    </a>
+                </div><!-- #item-header-avatar -->
+                <div id="item-header-content">
+
+                <h2>
+                    <a href="<?php bp_displayed_user_link(); ?>"><?php bp_displayed_user_fullname(); ?></a>
+                </h2>
+            
+                <?php if ( bp_is_active( 'activity' ) && bp_activity_do_mentions() ) : ?>
+                    <span class="user-nicename">@<?php bp_displayed_user_mentionname(); ?></span>
+                <?php endif; ?>
+            
+                <span class="activity"><?php bp_last_activity( bp_displayed_user_id() ); ?></span>
+            
+                <?php do_action( 'bp_before_member_header_meta' ); ?>
+            
+                <div id="item-meta">
+            
+                    <?php if ( bp_is_active( 'activity' ) ) : ?>
+            
+                        <div id="latest-update">
+            
+                            <?php bp_activity_latest_update( bp_displayed_user_id() ); ?>
+            
+                        </div>
+            
+                    <?php endif; ?>
+            
+                    <div id="item-buttons">
+            
+                        <?php //do_action( 'bp_member_header_actions' ); ?>
+            
+                    </div><!-- #item-buttons -->
+            
+                    <?php
+                    /***
+                     * If you'd like to show specific profile fields here use:
+                     * bp_member_profile_data( 'field=About Me' ); -- Pass the name of the field
+                     */
+                     do_action( 'bp_profile_header_meta' );
+            
+                     ?>
+            
+                </div><!-- #item-meta -->
+            
+            </div><!-- #item-header-content -->
+				<div class="item-list-tabs no-ajax" id="object-nav">
+					<ul>
+						<?php bp_get_displayed_user_nav(); ?>
+
+						<?php do_action( 'bp_member_options_nav' ); ?>
+					</ul>
+				</div>
+			</div>
+
+			<div id="item-body">
+
+				<h4><?php echo bp_word_or_name( __('Your Gifts', 'bp-gifts' ), __( "%s' Gifts", 'bp-gifts' )  ,false,false) ?></h4>
+				<div style="height: 30px; clear: both; display: block; height: 30px;"><!--  Divider --></div>
+				
+
+<?php if (is_user_logged_in() && !bp_is_my_profile()) {
+	
+	// Check if Cubepoints and cubepoints buddypress integration is alredy installed
+
+	 if ( function_exists( 'my_bp_gift_given_add_cppoints' ) || function_exists( 'mycred_get_settings' )) {
+		 
+		
+		if ( function_exists( 'my_bp_gift_given_add_cppoints') ){
+			$Mypoint = (int)cp_getPoints(cp_currentUser());		
+		} else {
+			global $current_user;
+     		get_currentuserinfo();
+			//$user_id = bp_displayed_user_id();
+			$user_id = $current_user->ID;
+			$mycred = mycred_get_settings();
+			$balance = $mycred->get_users_cred( $user_id );
+			//echo 'Your balance is ' . $mycred->format_creds( $balance );
+			$Mypoint = (int)$balance;
+		}
+		
+		$Giftpoints = (int)get_option('bp_gift_given_cp_bp');
+		
+		$CheckMyPoints = $Mypoint + $Giftpoints;
+		$PointNeedToSendGift = $Giftpoints - 2*$Giftpoints;
+	
+			if($CheckMyPoints<0){
+			?>
+				<div id="message" class="info">
+					
+				
+				<h4><?php _e('We are very sorry but it seems that you do not have enough points to sent a gift:','bp-gifts') ?></h4>
+				<p><?php _e('You need at least: ','bp-gifts'); echo $PointNeedToSendGift; _e(' Points','bp-gifts');?> </p>
+				
+				</div>
+				<div style="height: 30px; clear: both; display: block; height: 30px;"><!--  Divider --></div>
+			<?php	
+			}
+		
+			else
+			{ ?>
+
+				<div id="bpgifts-waiting" style="display:none"><img src="<?php echo plugins_url('/buddypress-gifts-latest-2014/includes/templates/css/loading.gif') ?>" /></div>
+				<div id="bpgifts-alert"></div><br />
+				<div class="sendgift-panel">
+
+				<div class="wrapper">
+                <br />
+				<div class="list_carousel">
+				<ul id="mycarousel" class="jcarousel-skin-tango">
+
+   					<!-- The content goes in here -->
+
+   					<?php
+
+   					$allgift = bp_gifts_allgift();
+
+   					foreach ($allgift as $giftitem) 
+   						{
+
+    					echo '<li><div><img class="giftitem" id="'.$giftitem->id.'" name="'.$giftitem->gift_name.'" src="'.plugins_url('/buddypress-gifts-latest-2014/includes/images/').$giftitem->gift_image.'" alt="'.$giftitem->gift_name.'" point="'.$giftitem->point.'" /></div><div>'.$giftitem->point.'</div>
+						</li>';
+						}?>
+
+				</ul>
+                <div class="clearfix"></div>
+				<a id="prev3" class="prev" href="#">&lt;</a>
+				<a id="next3" class="next" href="#">&gt;</a>
+				</div>
+				</div>
+
+				<br/>
+
+				<div class="sendgift-box">
+
+				<div class="giftbox">
+				
+				<img class="giftbox" id="999" name="emptybox" src="<?php echo plugins_url('/buddypress-gifts-latest-2014/includes/images/admin/emptybox.png') ?>" style="float:left; padding: 20px;"/>
+
+				</div>
+
+
+
+				<div id="gift-message">
+
+					<div id="gift-textarea">
+						
+						<textarea name="gift-message" id="giftms" value="" cols="68" rows="5" ></textarea>
+
+					</div>
+
+
+
+					<div id="gift-button">
+
+						<span class="highlight button" id="sendgift-button" >Send Gift</span>
+
+					</div>
+
+				</div>
+
+				</div>
+
+			</div>
+			<div style="height: 30px; clear: both; display: block; height: 30px;"><!--  Divider --></div>
+		<?php } ?>
+			
+	<?PHP  } //end of checking function 
+	else 
+	{	?>		
+	
+		<div id="bpgifts-waiting" style="display:none"><img src="<?php echo plugins_url('/buddypress-gifts-latest-2014/includes/templates/css/loading.gif') ?>" /></div>
+		<div id="bpgifts-alert"></div><br />
+
+				
+
+		<div class="sendgift-panel">
+
+		<div class="wrapper">
+        <br />
+		<div class="list_carousel">
+
+		<ul id="mycarousel" class="jcarousel-skin-tango">
+
+   		<!-- The content goes in here -->
+
+   		<?php
+
+   		$allgift = bp_gifts_allgift();
+
+   		foreach ($allgift as $giftitem) {
+
+    		echo '<li><div><img class="giftitem" id="'.$giftitem->id.'" name="'.$giftitem->gift_name.'" src="'.plugins_url('/buddypress-gifts-latest-2014/includes/images/').$giftitem->gift_image.'" alt="'.$giftitem->gift_name.'" point="'.$giftitem->point.'" /></div><div>'.$giftitem->point.'</div></li>';
+
+			}
+
+			?>
+
+		</ul>
+        <div class="clearfix"></div>
+        <a id="prev3" class="prev" href="#">&lt;</a>
+        <a id="next3" class="next" href="#">&gt;</a>
+		</div>
+		</div>
+
+			<br/>
+
+			<div class="sendgift-box">
+		
+			<div class="giftbox">
+			
+			<img class="giftbox" id="999" name="emptybox" src="<?php echo plugins_url('/buddypress-gifts-latest-2014/includes/images/admin/emptybox.png') ?>" style="float:left; padding: 20px;"/>
+
+			</div>
+
+
+
+			<div id="gift-message">
+
+				<div id="gift-textarea">
+					
+					<textarea name="gift-message" id="giftms" value="" style="overflow:hidden" cols="68" rows="5" ></textarea>
+
+				</div>
+
+
+
+				<div id="gift-button">
+
+					<span class="bp-gift-highlight button" id="sendgift-button" >Send Gift</span>
+
+				</div>
+
+			</div>
+
+			</div>
+
+		</div>
+		<div style="height: 30px; clear: both; display: block; height: 30px;"><!--  Divider --></div>
+		<br class="clear" />
+	
+	<?php } ?>
+	
+	<?php } ?>
+
+<!--------------------- display gift activity loop --------------------------------->
+
+
+
+<?php if ( bp_has_activities('scope=mentions&action=new_gifts&display_comments=threaded') ) : ?>
+
+
+
+	<div class="pagination">
+
+		<div class="pag-count"><?php bp_activity_pagination_count() ?></div>
+
+		<div class="pagination-links"><?php bp_activity_pagination_links() ?></div>
+
+	</div>
+
+
+
+	<ul id="activity-stream" class="activity-list item-list" style="list-style-type: none;">
+
+
+
+	<?php while ( bp_activities() ) : bp_the_activity(); ?>
+
+
+
+		<li class="<?php bp_activity_css_class(); ?>" id="activity-<?php bp_activity_id(); ?>">
+
+
+			
+            <div class="activity-avatar">
+				<a href="<?php bp_activity_user_link(); ?>">
+
+				<?php bp_activity_avatar(); ?>
+
+				</a>
+			</div>
+
+
+			<div class="activity-content">
+
+
+
+				<div class="activity-header">
+
+				<?php bp_activity_action(); ?>
+
+				</div>
+                
+                <?php if ( 'activity_comment' == bp_get_activity_type() ) : ?>
+
+				<div class="activity-inreplyto">
+				<strong><?php _e( 'In reply to: ', 'buddypress' ); ?></strong><?php bp_activity_parent_content(); ?> <a href="<?php bp_activity_thread_permalink(); ?>" class="view" title="<?php _e( 'View Thread / Permalink', 'buddypress' ); ?>"><?php _e( 'View', 'buddypress' ); ?></a>
+			</div>
+
+				<?php endif; ?>
+
+
+
+				<?php if ( bp_activity_has_content() ) : ?>
+
+				<div class="activity-inner">
+
+				<?php bp_activity_content_body(); ?>
+
+			</div>
+
+				<?php endif; ?>
+
+
+
+				<?php do_action( 'bp_activity_entry_content' ) ?>
+
+				
+
+				
+
+			</div>
+
+		<?php /*activity comments*/ ?>
+        
+				<?php do_action( 'bp_before_activity_entry_comments' ); ?>
+
+	<?php if ( ( is_user_logged_in() && bp_activity_can_comment() ) || bp_is_single_activity() ) : ?>
+
+		<div class="activity-comments">
+
+			<?php bp_activity_comments(); ?>
+
+			<?php if ( is_user_logged_in() ) : ?>
+
+				<form action="<?php bp_activity_comment_form_action(); ?>" method="post" id="ac-form-<?php bp_activity_id(); ?>" class="ac-form"<?php bp_activity_comment_form_nojs_display(); ?>>
+					<div class="ac-reply-avatar"><?php bp_loggedin_user_avatar( 'width=' . BP_AVATAR_THUMB_WIDTH . '&height=' . BP_AVATAR_THUMB_HEIGHT ); ?></div>
+					<div class="ac-reply-content">
+						<div class="ac-textarea">
+							<textarea id="ac-input-<?php bp_activity_id(); ?>" class="ac-input" name="ac_input_<?php bp_activity_id(); ?>"></textarea>
+						</div>
+						<input type="submit" name="ac_form_submit" value="<?php _e( 'Post', 'buddypress' ); ?>" /> &nbsp; <?php _e( 'or press esc to cancel.', 'buddypress' ); ?>
+						<input type="hidden" name="comment_form_id" value="<?php bp_activity_id(); ?>" />
+					</div>
+
+					<?php do_action( 'bp_activity_entry_comments' ); ?>
+
+					<?php wp_nonce_field( 'new_activity_comment', '_wpnonce_new_activity_comment' ); ?>
+
+				</form>
+
+			<?php endif; ?>
+
+		</div>
+
+	<?php endif; ?>
+
+	<?php do_action( 'bp_after_activity_entry_comments' ); ?>
+
+				
+				
+			
+			
+			
+			
+			<?php /*
+			
+			<!-- activiti comment -->
+			
+			<?php do_action( 'bp_before_activity_entry_comments' ); ?>
+
+			<?php if ( ( is_user_logged_in() && bp_activity_can_comment() ) || bp_activity_get_comment_count() ) : ?>
+
+			<div class="activity-comments">
+
+			<?php bp_activity_comments(); ?>
+
+			<?php if ( is_user_logged_in() ) : ?>
+
+				<form action="<?php bp_activity_comment_form_action(); ?>" method="post" id="ac-form-<?php bp_activity_id(); ?>" class="ac-form"<?php bp_activity_comment_form_nojs_display(); ?>>
+					<div class="ac-reply-avatar"><?php bp_loggedin_user_avatar( 'width=' . BP_AVATAR_THUMB_WIDTH . '&height=' . BP_AVATAR_THUMB_HEIGHT ); ?></div>
+					<div class="ac-reply-content">
+						<div class="ac-textarea">
+							<textarea id="ac-input-<?php bp_activity_id(); ?>" class="ac-input" name="ac_input_<?php bp_activity_id(); ?>"></textarea>
+						</div>
+						<input type="submit" name="ac_form_submit" value="<?php _e( 'Post', 'buddypress' ); ?>" /> &nbsp; <?php _e( 'or press esc to cancel.', 'buddypress' ); ?>
+						<input type="hidden" name="comment_form_id" value="<?php bp_activity_id(); ?>" />
+					</div>
+
+					<?php do_action( 'bp_activity_entry_comments' ); ?>
+
+					<?php wp_nonce_field( 'new_activity_comment', '_wpnonce_new_activity_comment' ); ?>
+
+				</form>
+
+			<?php endif; ?>
+
+		</div>
+
+			<?php endif; ?>
+
+			<?php do_action( 'bp_after_activity_entry_comments' ); ?>
+			
+			*/ ?>
+
+			
+
+			
+
+		</li>
+
+
+	<hr />
+	<?php endwhile; ?>
+
+
+
+	</ul>
+
+
+
+<?php else : ?>
+
+	<div id="message" class="info">
+
+		<p><?php echo bp_word_or_name( __('You still don\'t have any gift yet!', 'bp-gifts' ), __( "Either %s hasn't received any gift yet or they have restricted access", 'bp-gifts' )  ,false,false) ?></p>
+
+	</div>
+
+<?php endif; ?>
+
+
+
+
+
+<!-------------------------------------------------------------------->
+
+
+
+
+				</div>
+			</div><!-- #item-body -->
+
+		<?php do_action( 'bp_after_member_home_content' ); ?>
+
+	</article>
+	</div><!-- #content -->
+</div>
+</div>
+<?php get_sidebar( 'buddypress' ); ?>
+<?php get_footer( 'buddypress' ); ?>
+
---
+++ loader.php
@@ -0,0 +1,135 @@
+<?php
+
+
+
+/*
+Plugin Name: Buddypress Gifts Latest 2014
+Plugin URI: http://wordpress.org/extend/plugins/buddypress-gifts-latest-2014/
+Description: Third life of popular Gifts component for buddypress.
+Version: 1.5
+Revision Date: 06 04, 2015
+Requires at least: WP 3.3.1, BuddyPress 1.5.1
+Tested up to: WP 3.8.1, BuddyPress 1.9
+License: GNU General Public License 2.0 (GPL) http://www.gnu.org/licenses/gpl.html
+Author: Amreeta Ray
+Author URI: http://urseo.com/
+Site Wide Only: false
+*/
+
+/*************************************************************************************************************/
+/*
+define ( 'BP_GIFTS_IS_INSTALLED', 1 ); 
+define ( 'BP_GIFTS_VERSION', '1.9' );
+define ( 'BP_GIFTS_DB_VERSION', '1' );
+*/
+define ( 'BP_GIFTS_PLUGIN_DIR', dirname( __FILE__ ) );
+
+/* Only load the component if BuddyPress is loaded and initialized. */
+
+
+function bp_gifts_init() {
+
+	require( dirname( __FILE__ ) . '/includes/bp-gifts-core.php' );
+	if ( version_compare( BP_VERSION, '1.4', '>' ) )
+		require( dirname( __FILE__ ) . '/includes/bp-gifts-loader.php' );
+
+}
+
+
+//add_action( 'bp_init', 'bp_gifts_init' );
+add_action( 'bp_include', 'bp_gifts_init' );
+
+
+/* Put setup procedures to be run when the plugin is activated in the following function */
+
+function bp_gifts_activate() {
+
+	global $wpdb;
+
+	if ( !empty($wpdb->charset) )
+
+		$charset_collate = "DEFAULT CHARACTER SET $wpdb->charset";
+
+	$sql[] = "CREATE TABLE {$wpdb->base_prefix}bp_gifts (
+
+		  		id bigint(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+				gift_name varchar(100) NOT NULL,
+				gift_image varchar(100) NOT NULL,
+				category varchar(100) NOT NULL DEFAULT 'gift',
+				point decimal(10,5) NOT NULL DEFAULT '0',
+			    KEY gift_name (gift_name),
+				KEY category (category)
+		 	   ) {$charset_collate};";
+
+
+	require_once( ABSPATH . 'wp-admin/upgrade-functions.php' );
+
+
+	/**
+	* The dbDelta call is commented out so the example table is not installed.
+	* Once you define the SQL for your new table, uncomment this line to install
+	* the table. (Make sure you increment the BP_EXAMPLE_DB_VERSION constant though).
+	*/
+
+
+
+	dbDelta($sql);
+
+	/* insert gift images to table - place your image in images folder and remove default image if want */
+
+	if (get_site_option('bp-gifts-db-version') == '') { // if first install load image to gifts table
+
+	if ($handle = opendir( dirname( __FILE__ ) . '/includes/images/') ) {
+
+	    while (false !== ($imagefile = readdir($handle))) {
+
+		if (imagefile != 'admin' && imagefile != '' && $imagefile != '.' && imagefile != '..') {
+
+			$imagename = explode(".", $imagefile);
+
+	if ( $imagename[0] != 'admin' && $imagename[0] != '' ){
+
+        		$insert = $wpdb->prepare( "INSERT INTO " . $wpdb->base_prefix . "bp_gifts ( gift_name, gift_image ) VALUES ( %s, %s )", $imagename[0], $imagefile );
+
+		$results = $wpdb->query( $insert );
+
+	}
+
+		}
+
+        	    }
+
+    	}
+
+    	closedir($handle);
+
+	}
+
+
+	update_site_option( 'bp-gifts-db-version', BP_GIFTS_DB_VERSION );
+	
+	if ( !is_plugin_active('buddypress/bp-loader.php') || (!is_plugin_active('cubepoints/cubepoints.php') && !is_plugin_active('mycred/mycred.php')) ) {
+	// Deactivate the plugin
+	deactivate_plugins(__FILE__);
+	// Throw an error in the wordpress admin console
+	$error_message = __('This plugin requires <a href="https://wordpress.org/plugins/buddypress/" target="_blank">Buddypress</a> &amp; (<a href="http://wordpress.org/extend/plugins/cubepoints/" target="_blank">Cubepoints</a> OR <a href="https://wordpress.org/plugins/mycred/" target="_blank">MyCred</a>) plugins to be active!', 'buddypress');
+	die($error_message);
+	}
+
+}
+
+register_activation_hook( __FILE__, 'bp_gifts_activate' );
+
+/* On deacativation, clean up anything your component has added. */
+
+function bp_gifts_deactivate() {
+
+	/* You might want to delete any options or tables that your component created. */
+
+
+}
+
+register_deactivation_hook( __FILE__, 'bp_gifts_deactivate' );
+
+
+?>
